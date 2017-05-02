89f16ad1b312252b4d4d2cdace540e3df21163a1
hudatu <huchunhe32@163.com>
2017-05-02 19:16:42 -0400

    Check-Html Version 1.1

--- Check-Html.sh
+++ Check-Html.sh
@@ -2,10 +2,16 @@
 #Date : 2017-05-02
 #Author : Huchunhe
 #Check the html
-#Version : 1.0
+#Version : 1.1
 
 BaseDir="/var/www/html"
 BaseName="index.html"
 Mouth=`ll /var/www/html/index.html | awk '{print $6}'`
 Day=`ll /var/www/html/index.html | awk '{print $7}'`
 Time=`ll /var/www/html/index.html | awk '{print $8}'`
+
+if [ $Mouth != "May" || $Day -ne 2 || $Time != "18:36" ];then
+    echo "The index.html file was changed"
+else
+    echo "The index.html file not change"
+fi
